a1997650eca17f189aee47900e3b6c41d96714cc
Mingu Kang <kang.mingu94@gmail.com>
2021-06-29 22:15:27 +0900

    Update Init.java

--- src/main/java/app/Init.java
+++ src/main/java/app/Init.java
@@ -43,22 +43,6 @@ public class Init {
 		
 		String source = input;
 		
-//		String source = "public abstract class MisraTableViewColumn extends ColumnLabelProvider {\r\n" + 
-//				"  public abstract String getText(  Object element);\r\n" + 
-//				"  public abstract String getTitle();\r\n" + 
-//				"  public abstract int getWidth();\r\n" + 
-//				"  public TableViewerColumn addColumnTo(  TableViewer tableViewer){\r\n" + 
-//				"    TableViewerColumn tableViewerColumn=new TableViewerColumn(tableViewer,SWT.NONE);\r\n" + 
-//				"    TableColumn column=tableViewerColumn.getColumn();\r\n" + 
-//				"    column.setMoveable(false);\r\n" + 
-//				"    column.setResizable(false);\r\n" + 
-//				"    column.setText(getTitle());\r\n" + 
-//				"    column.setWidth(getWidth());\r\n" + 
-//				"    tableViewerColumn.setLabelProvider(this);\r\n" + 
-//				"    return tableViewerColumn;\r\n" + 
-//				"  }\r\n" + 
-//				"}";
-		
 			ASTSupportSingleton astTest = ASTSupportSingleton.getInstance();
 			List<Method> methodObjects = new ArrayList<Method>();
 			astTest.parse(source, new SourceCodeVisitor(methodObjects, source));
